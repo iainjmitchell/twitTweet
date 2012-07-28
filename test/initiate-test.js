d528e448508409268db19b156c0c669fa5d8a2b1
Iain J Mitchell <iainjmitchell@gmail.com>
2012-07-28 17:14:04 +0100

    ajax call made

--- test/initiate-test.js
+++ test/initiate-test.js
@@ -1,5 +1,19 @@
 (function($, undefined){
-	test('test', function(){
-		ok(1 == 1);
+	test('ajax call made', function(){
+		//given
+		var ajaxCallMade = false;
+		$.ajax = function(){
+			ajaxCallMade = true;
+		};
+		//when
+		$('#target').twitTweet();
+		//then
+		ok(ajaxCallMade);
 	});
 })(jQuery);
+
+$.widget("ijm.twitTweet", {
+	_create: function(){
+		$.ajax();
+	}
+});
